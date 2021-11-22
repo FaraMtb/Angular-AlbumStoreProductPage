ef8f56d6b2ddc79d6dee9a42f974594871fd26ff
FaraMtb <54081239+FaraMtb@users.noreply.github.com>
2021-11-22 20:45:21 +0200

    first new

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,9 @@ export class ProductService {
   constructor(private _http: Http) {
 
   }
-  getAlbum(id:number){
+  getAlbum(id:number) : Observable<Album>{
+
     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
+
   }
 }
