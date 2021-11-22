1fa4a3a3a98675dc840605c7c140f1f8e85dff72
FaraMtb <54081239+FaraMtb@users.noreply.github.com>
2021-11-22 20:41:11 +0200

    first new

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http,Response} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {Album} from './album';
 
 @Injectable()
 export class ProductService {
@@ -12,6 +13,6 @@ export class ProductService {
 
   }
   getAlbum(id:number){
-    return this._http.get(this._albumUrl).map((response)=>response.json());
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
 }
